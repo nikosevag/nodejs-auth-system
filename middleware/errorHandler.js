3d1f85de16b1c307364c1248d25a23cb6de76c8a
Nikos Evag <nikosevag.dev@gmail.com>
2023-07-09 18:44:19 +0300

    Handle Validation Error

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,7 @@
 const config = require('../config/config');
 function errorHandler(err, req, res, next) {
-  console.log('ERROR HANDLER -> ' + err);
+  console.log('ERROR HANDLER -> ');
+  console.log(err);
   if (err.name === 'TokenExpiredError') {
     return res.status(401).json({
       success: true,
@@ -15,6 +16,19 @@ function errorHandler(err, req, res, next) {
       error: null,
     });
   }
+
+  if (err.name === 'ValidationError') {
+    let errors = {};
+    Object.keys(err.errors).forEach((key) => {
+      errors[key] = err.errors[key].message;
+    });
+    return res.status(400).json({
+      success: true,
+      msg: null,
+      error: errors,
+    });
+  }
+
   if (config.environment === 'development') {
     return res.status(500).json({
       success: false,
